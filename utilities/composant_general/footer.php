b03e9872de4e96f32de0a87b3eb2f3d172ee3457
Ligia Aranda <arandaligia8@gmail.com>
2024-03-18 20:42:32 +0100

    footer/navbar

---
+++ utilities/composant_general/footer.php
@@ -0,0 +1,83 @@
+<?php include_once 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer Pauline</title>
+    <link></link>
+    <link rel="stylesheet" href="/style.css">
+    <link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous"></script>
+
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "LegalService",
+      "name": "Pauline Rhoder Avocate et Formatrice",
+      "description": "Pauline Rhoder, avocate et formatrice, offre des services juridiques en droit des personnes en milieu hospitalier psiquiatrique. Contactez notre avocate spécialisée dès aujourd'hui.",
+      "openingHours": "Mo-Fr 09:00-17:00",
+      "paymentAccepted": "Cash, Credit Card",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "127 Rue de Rome",
+        "addressLocality": "Marseille",
+        "postalCode": "13006",
+        "addressCountry": "France"
+      },
+      "telephone": "+33 7 72 06 23 77",
+      "email": "contact@rhoder-avocate.fr",
+      "priceRange": "€"
+    }
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
+</main>
+<!-- mettre le overflow en bootstrap -->
+
+<div class="container-fluid bg-bk">
+    <footer class="pt-5" style="overflow-x: hidden;">
+        <div class="row justify-content-center">
+            <div class="col-md-2 mb-3  text-center">
+                <h5 class="mb-5">Mentions légales</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Voir plus</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 mb-3 d-md-flex align-items-center justify-content-center" style="color: #0010A3">
+                <div class="vr d-none d-md-block"></div>
+                <hr class="d-md-none flex-grow-1">
+            </div>
+            <div class="col-md-2 mb-3  text-center">
+                <h5 class="mb-5">Contact</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2">127 rue de Rome, Marseille, France</li>
+                    <li class="nav-item mb-2">Tél. : + 33 (0)7 72 06 23 77</li>
+                    <li class="nav-item mb-2">www.rhenter-avocate.fr/</li>
+                    <li class="nav-item mb-2">contact@paulinerhenter-avocat.fr</li>
+                </ul>
+            </div>
+            <div class="col-md-2 mb-3 d-md-flex align-items-center justify-content-center" style="color: #0010A3">
+                <div class="vr d-none d-md-block"></div>
+                <div class="border-top d-md-none "></div>
+            </div>
+            <div class="col-md-2 mb-3 p-0  d-flex justify-content-center">
+                <img src="C:\wamp64\www\pauline_website\assets\logos\logoFondRemoved.png" class="img-fluid mb-2 col-4 col-md-12" alt="logo footer"   >
+            </div>
+        </div>
+    <div class="container-fluid">
+        <div class="d-flex flex-column flex-sm-row justify-content-center py-4 my-4 border-top">
+            <p class="text-center">© 2024 Sonia Tavares et Ligia Aranda, Inc. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+</div>
+        
---
+++ utilities/navigation/navbar.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer Pauline</title>
+    <link></link>
+    <link rel="stylesheet" href="/style.css">
+    <link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous"></script>
+
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "LegalService",
+      "name": "Pauline Rhoder Avocate et Formatrice",
+      "description": "Pauline Rhoder, avocate et formatrice, offre des services juridiques en droit des personnes en milieu hospitalier psiquiatrique. Contactez notre avocate spécialisée dès aujourd'hui.",
+      "openingHours": "Mo-Fr 09:00-17:00",
+      "paymentAccepted": "Cash, Credit Card",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "127 Rue de Rome",
+        "addressLocality": "Marseille",
+        "postalCode": "13006",
+        "addressCountry": "France"
+      },
+      "telephone": "+33 7 72 06 23 77",
+      "email": "contact@rhoder-avocate.fr",
+      "priceRange": "€"
+    }
+    </script>
+</head>
+<nav class="bg-bk navbar navbar-expand-lg border center">
+    <div class="container-fluid d-flex justify-content-center col-8">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav d-flex w-100 justify-content-center space-around">
+                <div class="row w-100 text-center d-flex justify-content-center">
+                    <div class="col-md">
+                        <li class="nav-item flex-grow-1">
+                            <a class="nav-link active" aria-current="page" href="../index.php">Accueil</a>
+                        </li>
+                    </div>
+                    <div class="col-md">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Avocate
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">J'ai besoin d'une avocate</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Cas pratiques</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Honoraires</a></li>
+                            </ul>
+                        </li>
+                    </div>
+                    <div class="col-md">
+                    <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Formatrice
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">J'ai besoin de me former</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">FAQ</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Demander un devis</a></li>
+                            </ul>
+                        </li>
+                    </div>
+                    <div class="col-md">
+                        <li class="nav-item flex-grow-1">
+                            <a class="nav-link" aria-disabled="true" href="../page_conseils.php">Conseils</a>
+                        </li>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </div>
+</nav>
