79f00c3bd7a48172bd5deb2659859b67147568fd
Sonycodes <soniatavaresebirdll@gmail.com>
2024-03-25 13:34:22 +0100

    page_formation-add

--- utilities/autres_composants/page_juridique/competences.php
+++ utilities/autres_composants/page_juridique/competences.php
@@ -2,15 +2,15 @@
 
 
 <section class="py-5 section_juridique" style="overflow: hidden;">
-    <div class="titre bg-red wh-100 front py-3">
+    <div class="bg-red wh-100 front py-3">
         <h1 class="text-center " style="z-index: 1;">
             Domaines de compétences
         </h1>
     </div>
     <div class="rotate bg-light-90-beige blur-beige" style="overflow: hidden;">
-    <div class=" container container-lg rotate-minus20 " >
-  <div class="">
-    <p class="h5 text-red" >
+        <div class=" container container-lg rotate-minus20 ">
+            <div class="">
+                <p class="h5 text-red">
                     Ma pratique est essentiellement centrée sur la défense des droits des personnes accompagnées en psychiatrie, et plus particulièrement de leur droit de contester des soins contraints ou de demander réparation pour des préjudices subis du fait de soins imposés.
 
                 </p>
@@ -18,18 +18,101 @@
                     J’interviens plus généralement s’agissant des mesures restrictives de liberté en lien avec la condition de « patient en psychiatrie » :
                 </p>
                 <ul class="text-red h5">
-                    <li class="d-flex text-red align-items-start mt-3"><span class="me-2">•</span>protection des majeurs (tutelle ou curatelle),</li>
-                    <li class="d-flex  text-red align-items-start"><span class="me-2">•</span>accès au statut de personne en situation de handicap</li>
+                    <li class="d-flex align-items-start mt-3"><span class="me-2 text-red">•</span>
+                        <p class=" text-red">protection des majeurs (tutelle ou curatelle), </p>
+                    </li>
+                    <li class="d-flex align-items-start"><span class="me-2 text-red">•</span>
+                        <p class=" text-red ">accès au statut de personne en situation de handicap </p>
+                    </li>
                 </ul>
                 <p class="h5 text-red ">
                     Par ailleurs, dès mon entrée dans la profession, j’ai intégré les listes de défense pénale d’urgence, de défense des victimes et de défense des mineurs pour assurer le rôle d’avocat commis d’office.
                 </p>
-  </div>
-</div>
-</div>
+            </div>
+        </div>
+    </div>
     <!-- ca va probablement dans le css -->
     <img src="../../../assets/deco/deco_bulle.svg" alt="Bulle gauche" class="bubble left">
     <img src="../../../assets/deco/deco_bulle.svg" alt="Bulle droite" class="bubble right">
+</section>
+
+<!-- section  honoraires -->
+
+<section class="py-5 section_juridique" style="overflow: hidden;">
+    <div class="bg-light-90-beige  wh-100 front py-3">
+        <h1 class="text-center text-red" style="z-index: 1;">
+            Domaines de compétences
+        </h1>
+    </div>
+    <div class="rotate-10 bg-red blur-red" style="overflow: hidden;">
+        <div class="container container-lg rotate-20 ">
+            <div class="border border-light border-3 m-5 row">
+                <p class="h5 text-red text-center p-5">
+
+                     Prix indicatif de la consultation de 30 mn à 1h : 50 à 75 euros HT
+                    -    <br>
+                    Si un travail est nécessaire au delà d’une consultation, la convention d’honoraires définit, avec votre accord le prix de l’assistance, du conseil, de la rédaction d’actes juridiques et des plaidoiries.
+                    Les honoraires sont ajustés en fonction de vos revenus, de la matière ainsi que de la complexité du dossier ou de la procédure. 
+                      <br>
+                    Vous avez peut-être une assurance de protection juridique susceptible de couvrir les frais. Qu’est-ce qu’une assurance de protection juridique ? https://www.service-public.fr/particuliers/vosdroits/F3049
+                     <br>
+                    Si vous n’avez pas d’assurance de protection juridique, il existe l’aide juridictionnelle, qui est une aide financière destinée à toute personne dont les ressources ne dépassent pas un certain montant.
+                    Pour savoir si vous y avez droit : https://www.service-public.fr/particuliers/vosdroits/F18074</p>
+                    <button class="text-center text-red" style="width: fit-content;">hello</button>
+            </div>
+        </div>
+    </div>
+</section>
 
 
+<section class="py-5 section_juridique" style="overflow: hidden;">
+    <div class="bg-red wh-100 front py-3">
+        <h1 class="text-center " style="z-index: 1;">
+            Domaines de compétences
+        </h1>
+    </div>
+    <div class="rotate bg-light-90-beige blur-beige" style="overflow: hidden;">
+        <div class=" container container-lg rotate-minus20 ">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+    </div>
+    <!-- ca va probablement dans le css -->
+    <img src="../../../assets/deco/deco_bulle.svg" alt="Bulle droite" class="bubble right">
 </section>
